20f6dcd4d039b91fe96287b49417c1afcfbb8588
hazem hamdy <hh4morsy@gmail.com>
2023-11-26 16:23:26 +0200

    commit for dbpost

--- core/settings.py
+++ core/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'login_user',
+        'USER': 'default',
+        'PASSWORD': '0qe8HQNRiwJp',
+        'HOST': 'ep-aged-silence-54497879-pooler.us-east-1.postgres.vercel-storage.com',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
